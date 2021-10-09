4d6a92026867a2ed9a04096411b2dbd7c65f487a
nasyhab <nostalmulti@gmail.com>
2021-10-09 17:39:17 +0700

    - Setup for card prefabs & json level data 1-2

---
+++ sidomath_smart_cards/prefabs/card-level1.go
@@ -0,0 +1,40 @@
+embedded_components {
+  id: "card-base"
+  type: "sprite"
+  data: "tile_set: \"/sidomath_smart_cards/atlas/atlas_gameplay-elements/gameplay-elements.atlas\"\n"
+  "default_animation: \"card-mockup\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: -0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "card-content"
+  type: "sprite"
+  data: "tile_set: \"/sidomath_smart_cards/tilesource/levels/card-sprites/card-level1.atlas\"\n"
+  "default_animation: \"type1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
